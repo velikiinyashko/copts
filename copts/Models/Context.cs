d9ed8cdd85e4ecde217a0618bfc193a4478e9836
chii chobits <velikiinyashko@live.ru>
2017-11-26 02:45:54 +0700

    start

---
+++ copts/Models/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace copts.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Companys> Companys { get; set; }
+
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ copts/Models/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace copts.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public int? CompanyId { get; set; }
+        public Company Company { get; set; }
+    }
+}
